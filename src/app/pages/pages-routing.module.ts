430ec77d6c6d4cdb977ca960d5a69a939487045b
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-14 13:46:03 +0500

    updates

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    data: { breadcrumb: 'Home' },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
