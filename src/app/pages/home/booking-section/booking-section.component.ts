ded7c1c6bc6c6bd7d11ecf5e79c2b9a1634ee19e
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-17 12:02:32 +0500

    fi=ormSUbmit

--- src/app/pages/home/booking-section/booking-section.component.ts
+++ src/app/pages/home/booking-section/booking-section.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { NetworkService } from '../../../services/network.service';
 
 @Component({
   selector: 'app-booking-section',
   standalone: false,
-  
+
   templateUrl: './booking-section.component.html',
   styleUrl: './booking-section.component.scss'
 })
 export class BookingSectionComponent {
+
+  formData = {
+    no_of_guests: '',
+    date: '',
+    time: ''
+  }
+
+  constructor(private network: NetworkService){
+
+  }
+
+  formSubmit(){
+    console.log(this.formData)
+
+  }
+
+
+
 
 }
