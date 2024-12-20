d212fc342c973b10f11f89ca1b46f71b4b05a2d7
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-20 21:24:31 +0500

    how na

--- src/app/pages/base-page/base-page.ts
+++ src/app/pages/base-page/base-page.ts
@@ -2,6 +2,7 @@ import { Injector } from "@angular/core";
 import { EventsService } from "../../services/events.service";
 import { NavService } from "../../services/nav.service";
 import { UtilityService } from "../../services/utility.service";
+import { NetworkService } from "../../services/network.service";
 
 
 export abstract class BasePage{
@@ -9,11 +10,13 @@ export abstract class BasePage{
   public nav: NavService;
   public utility: UtilityService;
   public events: EventsService;
+  public network: NetworkService;
 
   constructor(injector: Injector) {
     this.nav = injector.get(NavService);
     this.utility = injector.get(UtilityService);
     this.events = injector.get(EventsService);
+    this.network = injector.get(NetworkService);
 
   }
 
