443e7283e24c739f55c4cc1f7400ca90f47a442b
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-02-03 20:33:20 +0500

    BOUY

--- src/app/components/product-box/product-box.module.ts
+++ src/app/components/product-box/product-box.module.ts
@@ -1,8 +1,6 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductBoxComponent } from './product-box.component';
-
-
 
 @NgModule({
   declarations: [
@@ -13,6 +11,9 @@ import { ProductBoxComponent } from './product-box.component';
   ],
   exports: [
     ProductBoxComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class ProductBoxModule { }
