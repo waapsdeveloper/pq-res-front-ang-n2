7072e53fc9561c0503bdf3601d0ec2ce1d8dc196
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-02-03 17:19:28 +0500

    s

--- src/app/pages/splash/splash.component.ts
+++ src/app/pages/splash/splash.component.ts
@@ -4,48 +4,55 @@ import { BasePage } from '../base-page/base-page';
 @Component({
   selector: 'app-splash',
   standalone: false,
-
   templateUrl: './splash.component.html',
   styleUrl: './splash.component.scss',
 })
 export class SplashComponent extends BasePage {
   restaurant_id: any;
+
   constructor(injector: Injector) {
     super(injector);
     this.initialize();
   }
 
   async initialize() {
-    // this.showLoader();
-    this.restaurant_id = localStorage.getItem("restaurant_id")
- //   const defaults = await this.network.getDefaultRestaurantId();
-    const res = await this.network.restaurantDetail(this.restaurant_id);
-    console.log(res.data);
-    if (res && res.data){
-      let  R = res.data;
+    this.restaurant_id = localStorage.getItem("restaurant_id");
+
+    if (!this.restaurant_id) {
+      // If restaurant_id is not found in local storage, fetch default restaurant
+      const defaults = await this.network.getDefaultRestaurantId();
+      if (defaults && defaults.active_restaurant) {
+        let R = defaults.active_restaurant;
+        this.restaurant_id = R.id;
         localStorage.setItem('restaurant', JSON.stringify(R));
         localStorage.setItem('restaurant_id', R.id);
       }
-    
-   // console.log(defaults);
-    // if (defaults && defaults.active_restaurant) {
-    //   let R = defaults.active_restaurant;
-    //   localStorage.setItem('restaurant', JSON.stringify(R));
-    //   localStorage.setItem('restaurant_id', R.id);
-    //   //   localStorage.setItem("restaurant_id" , JSON.stringify(defaults.active_restaurant));
-    // }
+    }
 
-    // // check if user logged in - if yes set its profile and cart
-    // // const user = this.users.getLoginUserFromApi() as any;
-    // // if(user){
-    // //   const res = await this.network.getCartFromApi({user_id: user.id});
-    // //   console.log(res);
-    // //   // this.carte.set(res.cart);
-    // // }
+    // Fetch restaurant details
+    await this.fetchRestaurantDetails(this.restaurant_id);
 
     setTimeout(() => {
       this.hideLoader();
       this.events.publish('open-link', { link: 'tabs' });
     }, 2000);
+  }
+
+  async fetchRestaurantDetails(restaurantId: string) {
+    const res = await this.network.restaurantDetail(restaurantId);
+    if (res && res.data) {
+      let R = res.data;
+      localStorage.setItem('restaurant', JSON.stringify(R));
+      localStorage.setItem('restaurant_id', R.id);
+    }
+  }
+
+  // Call this method when the user changes the restaurant
+  async onRestaurantChange(newRestaurantId: string) {
+    if (this.restaurant_id !== newRestaurantId) {
+      this.restaurant_id = newRestaurantId;
+      localStorage.setItem('restaurant_id', newRestaurantId);
+      await this.fetchRestaurantDetails(newRestaurantId);
+    }
   }
 }
