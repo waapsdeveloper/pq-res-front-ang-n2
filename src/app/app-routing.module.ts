e40900d5201736f7497477b4993fd5805c73bd92
Muhammad <mhammadadeel11@gmail.com>
2025-02-07 19:49:35 +0500

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 const routes: Routes = [
   // {
@@ -12,11 +14,22 @@ const routes: Routes = [
     loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule),
     data: { breadcrumb: '' },
   },
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled', // Scroll restoration ko enable karein
+    anchorScrolling: 'enabled', // Anchor scrolling ko enable karein (optional)
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    // Har route change par page ko top par scroll karein
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0); // Page ko top par scroll karein
+    });
+  }
+}
