2a5941e9db2311fe57624e6a45e00106b2e30ac7
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-04-23 01:24:19 +0500

    ss

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -2,23 +2,19 @@ import { Injectable } from '@angular/core';
 import { NetworkService } from './network.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
-
   countries: any[] = [];
 
-  constructor(private network: NetworkService) { }
+  constructor(private network: NetworkService) {}
 
   async getCountries(): Promise<any[]> {
-
-    if(this.countries.length !== 0){
+    if (this.countries.length !== 0) {
       return this.countries;
     }
     const res = await this.network.getCountries();
-    this.countries = res.countries;
+    this.countries = res?.countries;
     return this.countries;
   }
-
-
 }
