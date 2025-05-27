93c6e2c7c94c26fb567c66e02300624b0aaf1003
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2025-05-28 00:51:25 +0500

    update

--- src/app/pages/home/banner-section/banner-section.component.ts
+++ src/app/pages/home/banner-section/banner-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NetworkService } from '../../../services/network.service';
+import { GlobalDataService } from '../../../services/global-data.service';
 
 @Component({
   selector: 'app-banner-section',
@@ -10,16 +11,23 @@ import { NetworkService } from '../../../services/network.service';
   styleUrl: './banner-section.component.scss',
 })
 export class BannerSectionComponent implements OnInit {
-  data: any;
+  restaurant: any;
+  currency_symbol: string = '$';
   lowestPrice: number = 0;
 
-  constructor(public router: Router, private network: NetworkService) {}
+  constructor(public router: Router, private network: NetworkService, private globalData: GlobalDataService) {}
 
   async ngOnInit() {
     let json = localStorage.getItem('restaurant');
-    this.data = json ? JSON.parse(json) : null;
-    let res = await this.network.lowestPrice();
+    this.restaurant = json ? JSON.parse(json) : null;
 
+    this.globalData.getCurrencySymbol().subscribe((symbol) => {
+      this.currency_symbol = symbol || '$'; // Default to $ if symbol is not set
+    });
+
+
+
+    let res = await this.network.lowestPrice();
     this.lowestPrice = parseFloat(res?.products?.price);
     console.log(this.lowestPrice);
   }
