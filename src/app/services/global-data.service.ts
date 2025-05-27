1dc775c24fd497353aa6cbc9e28d1bfb3531cf74
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2025-05-28 00:40:37 +0500

    update global service

--- src/app/services/global-data.service.ts
+++ src/app/services/global-data.service.ts
@@ -1,12 +1,67 @@
 import { Injectable } from '@angular/core';
 import { NetworkService } from './network.service';
+import { NgSimpleStateBaseRxjsStore, NgSimpleStateStoreConfig } from 'ng-simple-state';
+import { Observable } from 'rxjs';
 
+export interface GlobalDataState {
+  restaurant_id: number;
+  currency: string;
+  currency_symbol: string;
+}
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalDataService {
+export class GlobalDataService extends NgSimpleStateBaseRxjsStore<GlobalDataState> {
 
-  constructor(private network: NetworkService) { }
+  private restaurantId: string | null = null;
+  private currency: string | null = null;
+  private currencySymbol: string | null = null;
+
+  constructor(private network: NetworkService) {
+    super();
+  }
+  protected storeConfig(): NgSimpleStateStoreConfig {
+    return {
+      storeName: 'GlobalDataState'
+    };
+  }
+  protected initialState(): GlobalDataState {
+    return {
+      restaurant_id: 0,
+      currency: 'USD',
+      currency_symbol: '$'
+    };
+  }
+
+  setRestaurantId(id: number): void {
+    this.restaurantId = id.toString();
+    this.setState((state) => ({ restaurant_id: id }));
+  }
+
+  getRestaurantId(): Observable<any>{
+    return this.selectState((state) => state.restaurant_id);
+  }
+
+  setCurrency(currency: string): void {
+    this.currency = currency;
+    this.setState((state) => ({ currency }));
+  }
+
+  getCurrency(): Observable<any> {
+    return this.selectState((state) => state.currency);
+  }
+
+  setCurrencySymbol(symbol: string): void {
+    this.currencySymbol = symbol;
+    this.setState((state) => ({ currency_symbol: symbol }));
+  }
+
+  getCurrencySymbol(): Observable<any> {
+    return this.selectState((state) => state.currency_symbol);
+  }
+
+
+
 
   setRestaurantData(id: any): Promise<any> {
 
@@ -36,6 +91,9 @@ export class GlobalDataService {
       let R = res.data;
       localStorage.setItem('restaurant', JSON.stringify(R));
       localStorage.setItem('restaurant_id', R.id);
+
+      this.setRestaurantId(R.id);
+
     }
 
     const config = await this.network.getRestaurantConfigById(restaurantId);
@@ -43,6 +101,8 @@ export class GlobalDataService {
 
     if (config?.data) {
       localStorage.setItem('restaurant_config', JSON.stringify(config.data));
+      this.setCurrency(config.data.currency);
+      this.setCurrencySymbol(config.data.currency_symbol);
     }
 
     return res.data;
